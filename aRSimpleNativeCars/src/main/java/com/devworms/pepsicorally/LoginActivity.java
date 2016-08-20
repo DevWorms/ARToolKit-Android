2da42f693cd0fd892181c09b71a2b1374096813a
sergio ivan lopez monzon <sergioivan154@gmail.com>
2016-08-19 19:01:44 -0500

    push notification

--- aRSimpleNativeCars/src/main/java/com/devworms/pepsicorally/LoginActivity.java
+++ aRSimpleNativeCars/src/main/java/com/devworms/pepsicorally/LoginActivity.java
@@ -1,15 +1,23 @@
 package com.devworms.pepsicorally;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.devworms.pepsicorally.mobile.AWSMobileClient;
+import com.devworms.pepsicorally.mobile.user.IdentityManager;
 
 import org.artoolkit.ar.samples.ARSimpleNativeCars.R;
 
@@ -25,6 +33,7 @@ public class LoginActivity extends Activity {
 
     // Progress Dialog
     private ProgressDialog pDialog;
+    private IdentityManager identityManager;
 
     //  Datos Usuario
     EditText mail;
@@ -43,6 +52,16 @@ public class LoginActivity extends Activity {
 
         misPrefs = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
 
+
+        // Obtain a reference to the mobile client. It is created in the Application class,
+        // but in case a custom Application class is not used, we initialize it here if necessary.
+        AWSMobileClient.initializeMobileClientIfNecessary(this);
+
+        // Obtain a reference to the mobile client. It is created in the Application class.
+        final AWSMobileClient awsMobileClient = AWSMobileClient.defaultMobileClient();
+
+        // Obtain a reference to the identity manager.
+        identityManager = awsMobileClient.getIdentityManager();
     }
 
     public void pantallaRegistro(View view) {
@@ -133,5 +152,43 @@ public class LoginActivity extends Activity {
 
         }
 
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        final AWSMobileClient awsMobileClient = AWSMobileClient.defaultMobileClient();
+
+        // register notification receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(notificationReceiver,
+                new IntentFilter(PushListenerService.ACTION_SNS_NOTIFICATION));
+    }
+    private final BroadcastReceiver notificationReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("Push", "Received notification from local broadcast. Display it in a dialog.");
+
+            Bundle data = intent.getBundleExtra(PushListenerService.INTENT_SNS_NOTIFICATION_DATA);
+            String message = PushListenerService.getMessage(data);
+
+            new AlertDialog.Builder(LoginActivity.this)
+                    .setTitle(getString(R.string.push_title))
+                    .setMessage(message)
+                    .setPositiveButton(android.R.string.ok, null)
+                    .show();
+        }
+    };
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // unregister notification receiver
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(notificationReceiver);
     }
 }
--- aRSimpleNativeCars/src/main/java/com/devworms/pepsicorally/SplashInicioActivity.java
+++ aRSimpleNativeCars/src/main/java/com/devworms/pepsicorally/SplashInicioActivity.java
@@ -1,10 +1,18 @@
 package com.devworms.pepsicorally;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.devworms.pepsicorally.mobile.AWSMobileClient;
+import com.devworms.pepsicorally.mobile.user.IdentityManager;
 
 import org.artoolkit.ar.samples.ARSimpleNativeCars.R;
 
@@ -13,10 +21,24 @@ public class SplashInicioActivity extends Activity {
     //  Preferencias
     SharedPreferences misPrefs;
 
+    private IdentityManager identityManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_inicio);
+
+
+
+        // Obtain a reference to the mobile client. It is created in the Application class,
+        // but in case a custom Application class is not used, we initialize it here if necessary.
+        AWSMobileClient.initializeMobileClientIfNecessary(this);
+
+        // Obtain a reference to the mobile client. It is created in the Application class.
+        final AWSMobileClient awsMobileClient = AWSMobileClient.defaultMobileClient();
+
+        // Obtain a reference to the identity manager.
+        identityManager = awsMobileClient.getIdentityManager();
 
 
         misPrefs = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
@@ -57,6 +79,42 @@ public class SplashInicioActivity extends Activity {
     protected void onPause() {
         // TODO Auto-generated method stub
         super.onPause();
+
+        // unregister notification receiver
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(notificationReceiver);
         finish();
     }
+
+
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        final AWSMobileClient awsMobileClient = AWSMobileClient.defaultMobileClient();
+
+        // register notification receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(notificationReceiver,
+                new IntentFilter(PushListenerService.ACTION_SNS_NOTIFICATION));
+    }
+    private final BroadcastReceiver notificationReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("Push", "Received notification from local broadcast. Display it in a dialog.");
+
+            Bundle data = intent.getBundleExtra(PushListenerService.INTENT_SNS_NOTIFICATION_DATA);
+            String message = PushListenerService.getMessage(data);
+
+            new AlertDialog.Builder(SplashInicioActivity.this)
+                    .setTitle(getString(R.string.push_title))
+                    .setMessage(message)
+                    .setPositiveButton(android.R.string.ok, null)
+                    .show();
+        }
+    };
+
+
 }
